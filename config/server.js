e7468988034159a7e6f8f374d23fd2cddbe20ad4
guozi <54030955+mimihhhh8@users.noreply.github.com>
2021-09-17 14:25:35 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd56db6512d11ea68d822d95f5c32c825'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55402fd9-b01d-4af3-8699-70d3613bf5a3'
+};
